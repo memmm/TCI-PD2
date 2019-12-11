9a3223159631e52af9197979793b544ce63137be
memmm <emesenge@gmail.com>
2019-12-11 20:54:29 +0100

    added test cases

--- src/test/java/RaceResultsServiceTest.java
+++ src/test/java/RaceResultsServiceTest.java
@@ -5,14 +5,15 @@ import static org.mockito.Mockito.*;
 
 public class RaceResultsServiceTest {
 
-    /**
-     * Example from the book (pages 80-84)
-     * */
+
     private RaceResultsService raceResults = new RaceResultsService();
     private Message message = mock(Message.class);
     private Client clientA = mock(Client.class, "clientA");
     private Client clientB = mock(Client.class, "clientB");
 
+    /**
+     * Example from the book (pages 80-84)
+     * */
     @Test
     public void notSubscribedClient_ShouldNotReceiveMessage() {
         raceResults.send(message);
@@ -20,12 +21,19 @@ public class RaceResultsServiceTest {
         verify(clientB, never()).receive(message);
     }
 
+    /**
+     * Example from the book (pages 80-84)
+     * */
     @Test
     public void subscribedClient_ShouldReceiveMessage() {
         raceResults.addSubscriber(clientA);
         raceResults.send(message);
         verify(clientA).receive(message);
     }
+
+    /**
+     * Example from the book (pages 80-84)
+     * */
     @Test
     public void allSubscribedClients_ShouldReceiveMessages() {
         raceResults.addSubscriber(clientA);
@@ -35,6 +43,9 @@ public class RaceResultsServiceTest {
         verify(clientB).receive(message);
     }
 
+    /**
+     * Example from the book (pages 80-84)
+     * */
     @Test
     public void multiSubscriber_shouldReceiveOnlyOneMessage() {
         raceResults.addSubscriber(clientA);
@@ -43,6 +54,9 @@ public class RaceResultsServiceTest {
         verify(clientA).receive(message);
     }
 
+    /**
+     * Example from the book (pages 80-84)
+     * */
     @Test
     public void unsubscribedClientShouldNotReceiveMessages() {
         raceResults.addSubscriber(clientA);
@@ -50,5 +64,47 @@ public class RaceResultsServiceTest {
         raceResults.send(message);
         verify(clientA, never()).receive(message);
     }
+
+    /* Race Results Enhanced - RaceResults should send messages with the results of different categories of race - e.g. horse
+      races, F1 races, boat-races, etc. Subscribers should be able to subscribe to selected categories. Make
+      sure they receive only messages related to the ones they have signed up for. */
+
+    // Mock object as dummy object
+    @Test
+    public void subscriber_AfterSubscribeToSelectedCategory_ShouldBeOnList() {
+
+    }
+
+    // Indirect output (test spy)
+    @Test
+    public void subscriber_AfterSubscribeToSelectedCategory_ShouldReceiveOnlyRelevantMessage() {
+
+    }
+
+    /**
+     * Indirect output
+     * */
+    @Test
+    public void subscriber_WhenSubscribedToMultipleCategories_ShouldReceiveMultipleMessages() {
+
+    }
+
+    /* Each message sent by RaceResultsService should be logged. Introduce a logging DOC, and make
+      sure that the date and text of each message is logged. Do not implement the logging mechanism:
+      concentrate on the interactions between the service and its collaborator */
+
+    /**
+     * Indirect output (stub) - SUT calls Message's getText() and getDate() getters and passes it to Logger's log() (Indirect input)
+     */
+    @Test
+    public void logger_WhenMessageIsSent_ShouldLog() {
+
+    }
+
+    /* In the tests implemented so far, RaceResultsService sends only one message. This is unrealistic!
+       Enhance the tests to ensure that subscribers will receive any number of sent messages. */
+
+    /* What should happen if a client that is not subscribed tries to unsubscribe? Make up your mind about
+       it, write a test which verifies this behaviour, and make RaceResultsService behave accordingly.*/
 
 }
