89e97cce79973d7ee8e2a0a6ef7aad1158c3cb06
memmm <emesenge@gmail.com>
2019-12-11 19:05:48 +0100

    first test now passes + JavaDoc added

--- src/main/java/RaceResultsService.java
+++ src/main/java/RaceResultsService.java
@@ -1,7 +1,15 @@
+/**
+ * A service whose role is to inform interested parties about the results of races.
+ * There is a notification service, which allows clients to subscribe.
+ * The service should send out messages to all of its subscribers
+ * */
 public class RaceResultsService {
 
+    private Client client;
     public void addSubscriber(Client client) {
+        this.client = client;
     }
     public void send(Message message) {
+        client.receive(message);
     }
 }
