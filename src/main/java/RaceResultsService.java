3ab0da93daba93374ec516b668f5ade34c1d336b
memmm <emesenge@gmail.com>
2019-12-11 21:38:39 +0100

    Code for multiple lists

--- src/main/java/RaceResultsService.java
+++ src/main/java/RaceResultsService.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * A service whose role is to inform interested parties about the results of races.
@@ -9,18 +10,28 @@ import java.util.HashSet;
  * */
 public class RaceResultsService {
 
-    private Collection<Client> clients = new HashSet<Client>();
+    private Collection<Client> F1MessagingList = new HashSet<Client>();
+    private Collection<Client> HorseRaceMessagingList = new HashSet<Client>();
+    private Collection<Client> BoatRaceMessagingList = new HashSet<Client>();
 
-    public void addSubscriber(Client client) {
-        clients.add(client);
+    public List<HashSet<Client>> getMessagingLists() {
+        List lists = new ArrayList<Collection<Client>>();
+        lists.add(F1MessagingList);
+        lists.add(HorseRaceMessagingList);
+        lists.add(BoatRaceMessagingList);
+        return lists;
     }
-    public void send(Message message) {
-        for (Client client : clients) {
+
+    public void addSubscriber(Client client, Collection<Client> list) {
+        list.add(client);
+    }
+    public void send(Message message, Collection<Client> list) {
+        for (Client client : list) {
             client.receive(message);
         }
     }
 
-    public void removeSubscriber(Client client) {
-        clients.remove(client);
+    public void removeSubscriber(Client client, Collection<Client> list) {
+        list.remove(client);
     }
 }
